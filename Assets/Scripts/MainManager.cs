46ba2460924f75c9f3aca36cadc26557250ebf62
Kuan Jia Jun <jiajun2838@gmail.com>
2024-04-19 00:06:36 +0800

    Revert "Update MainManager.cs"
    This reverts commit 67a82335024d38d3f30d1b1cddec91c8bdde90d4.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -19,7 +19,7 @@ public class MainManager : MonoBehaviour
     private bool m_GameOver = false;
 
     
-    //Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
         const float step = 0.6f;
